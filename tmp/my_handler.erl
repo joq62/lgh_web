ca8829e7f077a83d7144631b4cbe528fa0214286
joq62 <jakim.leche@gmail.com>
2023-11-09 00:56:47 +0100

    Deleted tmp

--- tmp/my_handler.erl
+++
@@ -1,9 +0,0 @@
--module(my_handler).
--export([init/2, handle/2]).
-
-init(Req, _Opts) ->
-    {ok, Req, undefined}.
-
-handle(Req, State) ->
-    {ok, Req2} = cowboy_req:reply(200, [], <<"Hello, Cowboy!">>, Req),
-    {ok, Req2, State}.
